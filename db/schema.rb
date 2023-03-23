6aaaec40a49006d88ad70d9ac36c7062af06893c
Marcel Arie <googlillo@gmail.com>
2023-03-23 23:41:15 +0100

    style: rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_23_223800) do
-  create_table "drivers", force: :cascade do |t|
-    t.string "name"
-    t.text "vehicle"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_230_323_223_800) do
+  create_table 'drivers', force: :cascade do |t|
+    t.string 'name'
+    t.text 'vehicle'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
